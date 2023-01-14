badca4281b13e9a51e817836b0499fb9e25db1b8
Jeremy Breedlove <breedjw@jwb-mbp.attlocal.net>
2023-01-13 22:54:29 -0500

    initial commit

---
+++ src/main/java/com/jwbreedlove/springdemos/SpringKubernetesConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.jwbreedlove.springdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubernetesConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKubernetesConfigserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jwbreedlove/springdemos/SpringKubernetesConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwbreedlove.springdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubernetesConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
